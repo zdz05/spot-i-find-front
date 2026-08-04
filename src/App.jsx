93190ab0568f14902acd926eb59f97b5528d0c49
zahid <309906859+zdz05@users.noreply.github.com>
2026-08-05 00:01:42 +0100

    Wire up app routing and navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import CountryChart from "./pages/CountryChart";
+import { Routes, Route } from "react-router-dom";
+import { SongProvider } from "./contexts/SongContext";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1>Spot-i-Find</h1>
-    </div>
+    <SongProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country/:slug" element={<CountryChart />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </SongProvider>
   );
 }
 
